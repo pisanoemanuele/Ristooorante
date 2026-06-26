d1aae55a60eb4a918c18d1fd0e9f70ff9eb73ee6
Emanuele Pisano <emanuele.pisano@mac.com>
2026-06-26 16:54:12 +0200

    Initial Commit

---
+++ Ristooorante/RistoooranteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RistoooranteApp.swift
+//  Ristooorante
+//
+//  Created by Emanuele Pisano on 26/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct RistoooranteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
