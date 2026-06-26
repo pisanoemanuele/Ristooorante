a6be4327bbc2f0c3dc60cc39ee6034c6ca1e2451
Emanuele Pisano <emanuele.pisano@mac.com>
2026-06-26 17:16:12 +0200

    004 - Aggiunto model Ristorante

---
+++ Ristooorante/Ristorante.swift
@@ -0,0 +1,22 @@
+//
+//  Ristorante.swift
+//  Ristooorante
+//
+//  Created by Emanuele Pisano on 26/06/2026.
+//
+
+import Foundation
+
+struct Ristorante: Codable, Identifiable {
+    let id: UUID
+    let nome: String
+    let indirizzo: String?
+    let lat: Double?
+    let lng: Double?
+    let telefono: String?
+    let sito_web: String?
+    let url_menu: String?
+    let citta_id: UUID
+    let approvato: Bool
+    let modalita_conferma: String
+}
