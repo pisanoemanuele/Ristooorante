820a18371ffd1746eefeff8d3ca4ccefeed7937a
Emanuele Pisano <emanuele.pisano@mac.com>
2026-06-26 17:14:05 +0200

    003 - Aggiunto SupabaseClient

---
+++ Ristooorante/SupabaseClient.swift
@@ -0,0 +1,14 @@
+//
+//  SupabaseClient.swift
+//  Ristooorante
+//
+//  Created by Emanuele Pisano on 26/06/2026.
+//
+
+import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+    supabaseURL: URL(string: "https://ljwvzicrgzjzqcnqtvxb.supabase.co")!,
+    supabaseKey: "sb_publishable_1Vvbz1SHf5Z6zWIBU5lF4A_o5q_4ean"
+)
